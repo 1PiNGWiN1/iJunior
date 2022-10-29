1e3a4b569d89fac4099ca859f8b196743df3b339
Admin <Admin@DESKTOP-V7LUC58>
2022-10-29 13:52:55 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string exitFromApp = "exit";
+
+            Console.Write("Сколько у вас рублей? ");
+            double balanceRUB = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Сколько у вас долларов? ");
+            double balanceUSD = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Сколько у вас евро? ");
+            double balanceEUR = Convert.ToSingle(Console.ReadLine());
+
+            double exchangeRateRubToUSD = 31.40;
+            double exchangeRateRubToEUR = 45.40;
+            double exchangeRateEURToUSD = 1;
+            double exchangeRateUSDToEUR = 1;
+            double exchangeRateEURToRUB = 0.016241;
+            double exchangeRateUSDToRUB = 0.016298;
+
+            Console.WriteLine("Что вы хотите обменять?");
+            Console.WriteLine("1. Рубли в доллары.| 2.Рубли в евро.| 3.Доллары в рубли.| 4.Доллары в евро.| 5.Евро в рубли.| 6.Евро в доллары.| Для выхода введите exit.");
+            Console.Write("Введите номер обменника или команду выхода: ");
+            string userInput = Console.ReadLine();
+            while (userInput != exitFromApp) 
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Сколько долларов вы хотите купить?");
+                        
+                        break;
+                    case "2":
+                        Console.WriteLine("Сколько евро вы хотите купить?");
+                        break;
+                    case "3":
+                        Console.WriteLine("Сколько рублей вы хотите купить?");
+                        break;
+                    case "4":
+                        Console.WriteLine("Сколько евро вы хотите купить?");
+                        break;
+                    case "5":
+                        Console.WriteLine("Сколько рублей вы хотите купить?");
+                        break;
+                    case "6":
+                        Console.WriteLine("Сколько долларов вы хотите купить?");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("MyFirstProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyFirstProject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a62f6453-05ca-473d-ba83-2cb731b94790")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
